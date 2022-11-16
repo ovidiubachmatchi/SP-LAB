7b02fc4891d7be517ef440c28d088ac1a606e7cd
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2022-11-16 12:21:05 +0200

    Lab 4

--- src/Image.java
+++ src/Image.java
@@ -1,8 +1,15 @@
+import java.util.concurrent.TimeUnit;
+
 public class Image implements Element {
     private String imageName;
 
     public Image(String imageName) {
         this.imageName = imageName;
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void print() {
---
+++ src/ImageProxy.java
@@ -0,0 +1,36 @@
+public class ImageProxy implements Element {
+    private String url;
+    private Image realImage;
+
+    public ImageProxy(String url) {
+        this.url = url;
+    }
+
+    private Image loadImage() {
+        if(realImage == null) {
+            realImage = new Image(url);
+        }
+        return realImage;
+    }
+
+    @Override
+    public void print() {
+        loadImage();
+        realImage.print();
+    }
+
+    @Override
+    public void add(Element e) {
+
+    }
+
+    @Override
+    public void remove(Element e) {
+
+    }
+
+    @Override
+    public void get(int e) {
+
+    }
+}
